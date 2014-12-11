6350c9a62db69f14b99f9c2adbd21a20b7ee8e90
Joshua McKenzie <whereyoucanemailme@gmail.com>
2014-12-10 23:41:00 -0600

    All methods tests

---
+++ tests.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once 'groove.class.php';
+
+// $groove = new groove('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+// $ticket = $groove->getTicket(1);
+// $tickets = $groove->getTickets();
+// $groove->setTicketState(1, 'opened');
+// $ticket_state = $groove->getTicketState(1);
+// $tickets = $groove->getTicketMessages(1);
+// $messages = $groove->getTicketMessages();
+// $message = $groove->getTicketMessage(1111111);
+// $groove->createTicketMessage(7, 'on another note, lets wait.');
+// $customers = $groove->getCustomers();
+// $customer = $groove->getCustomer('john@doe.xyz');
+// $groove->updateCustomer('john@doe.xyz', ['first_name'=>'Jon']);
+// $agent = $groove->getAgent('jane@doe.xyz');
+// $tickets = $groove->getTicketMessages(1);
+
+?>
